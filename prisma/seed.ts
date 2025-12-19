6acb3663eafdb0087c021db6426bc6b2b5beebb9
Ankit Devani <ankitdevani9779@gmail.com>
2025-12-19 19:36:54 +0530

    add commnad for seed data in user tabel

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,7 +9,12 @@ const adapter = new PrismaPg({
 const prisma = new PrismaClient({ adapter });
 
 async function main() {
-  const password = await bcrypt.hash("password123", 10);
+  if (process.env.SHOULD_SEED !== "true") {
+    console.log("Skipping seed");
+    return;
+  }
+
+  const password = await bcrypt.hash("password@123", 10);
 
   await prisma.user.upsert({
     where: { email: "admin@test.com" },
@@ -23,33 +28,44 @@ async function main() {
   });
 
   await prisma.user.upsert({
-    where: { email: "editor@test.com" },
-    update: {},
-    create: {
-      email: "editor@test.com",
-      name: "Editor",
-      password,
-      role: UserRole.EDITOR,
-    },
-  });
-
-  await prisma.user.upsert({
     where: { email: "editor1@test.com" },
     update: {},
     create: {
-      email: "editor@test.com",
-      name: "Editor",
+      email: "editor1@test.com",
+      name: "Editor 1",
       password,
       role: UserRole.EDITOR,
     },
   });
 
   await prisma.user.upsert({
-    where: { email: "user@test.com" },
+    where: { email: "editor2@test.com" },
     update: {},
     create: {
-      email: "user@test.com",
-      name: "User",
+      email: "editor2@test.com",
+      name: "Editor 2",
+      password,
+      role: UserRole.EDITOR,
+    },
+  });
+
+  await prisma.user.upsert({
+    where: { email: "user1@test.com" },
+    update: {},
+    create: {
+      email: "user1@test.com",
+      name: "User 1",
+      password,
+      role: UserRole.VIEWER,
+    },
+  });
+
+  await prisma.user.upsert({
+    where: { email: "user2@test.com" },
+    update: {},
+    create: {
+      email: "user2@test.com",
+      name: "User 2",
       password,
       role: UserRole.VIEWER,
     },
